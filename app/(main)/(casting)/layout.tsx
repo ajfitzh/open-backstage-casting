1fb66f6f3e62447f51fc60470c738644addb702d
Austin James Fitzhugh <austin.j.fitzhugh@gmail.com>
2026-01-25 12:02:11 -0500

    removed sidebars

--- app/(main)/(casting)/layout.tsx
+++ app/(main)/(casting)/layout.tsx
@@ -1,9 +1,7 @@
-import CastingSidebar from '@/app/components/CastingSidebar'; // Updated import
 
 export default function CastingLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-full w-full"> {/* CHANGED: h-screen -> h-full */}
-      <CastingSidebar /> 
       <main className="flex-1 overflow-auto bg-zinc-900/50 relative">
         {children}
       </main>
--- app/(main)/(staff)/layout.tsx
+++ app/(main)/(staff)/layout.tsx
@@ -1,9 +1,8 @@
-import StaffSidebar from '@/app/components/StaffSidebar'; 
+
 
 export default function StaffLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-full w-full"> {/* CHANGED: h-screen -> h-full */}
-      <StaffSidebar /> 
       <main className="flex-1 overflow-auto bg-zinc-900/50 relative">
         {children}
       </main>
