766b627149140d86718f4ade0461f5c99b26f3c0
Austin James Fitzhugh <austin.j.fitzhugh@gmail.com>
2026-01-25 12:05:08 -0500

    asdca

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-import GlobalHeader from '../components/GlobalHeader';
+import GlobalHeader from '@/app/components/GlobalHeader/GlobalHeader';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
